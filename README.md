5e81efd8f2c7e3555c9189cf7d7b8c1a037ce007
Carlos Rello <carlos.rello@outlook.com>
2021-10-01 19:17:34 -0500

    Challenge

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Mission-to-Mars
-Mars Project
+
+We are setting up a website with the latest information about Mars that contain a small extract of the last news,
+an updated picture of the planet and some mars facts.
+
+We decided to scrap information from the other web pages and set together a web page this is the look of the web page:
--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-#use Flask to render a template, redirecting to another url, and creating a URL.
 from flask import Flask, render_template, redirect, url_for
-#use PyMongo to interact with our Mongo database.
 from flask_pymongo import PyMongo
-#use the scraping code, we will convert from Jupyter notebook to Python.
 import scraping
 
-#set up Flask:
+# app = Flask(__name__, template_folder='.') # '.' means the current directory
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-#define the route for the HTML page
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
-#scraping route
+
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
@@ -25,6 +21,5 @@ def scrape():
    mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
 
-# Tell Flask to run
 if __name__ == "__main__":
    app.run()
